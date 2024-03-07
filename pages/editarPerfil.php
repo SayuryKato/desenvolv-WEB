45f21572b71d51d3abbb56f8a73754589b782512
Joao Filho <joaossfbae@gmail.com>
2024-03-06 20:45:42 -0400

    perfil

--- pages/editarPerfil.php
+++ pages/editarPerfil.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="../style/perfil.css">
     <link rel="stylesheet" href="../global.css">
 </head>
+
 <body>
     <?php
     // Inclui o arquivo de conexão
@@ -16,46 +17,64 @@
     ?>
         <main class="main-container">
             <div class="aside">
-                <?php 
-                    include("../components/aside.php");
+                <?php
+                include("../components/aside.php");
                 ?>
-                
+
             </div>
-            <h1 style="display: block; width: 100%">Meu perfil</h1>
-            <div class="div1">
-                <div class="img-container">
-                    <figure>
-                        <img class="perfil-img" src="../assets/perfil_joao.jpg" alt="img_joao">
-                    </figure>
-                    <a href="editarPerfil.php" class="button">Editar Perfil</a>
+            <form action="" style="width:80%">
+            
+                <div style="display:block">
+                    <div class="div1">
+                        <div class="img-container">
+                            <img class="perfil-img" src="../assets/perfil_joao.jpg" alt="img_joao">
+                        </div>
+                        <div class="input-container">
+                            <div class="div-label">
+                                <label for="nome">Nome </label>
+                                <input type="text" value="<?php echo $row["nome"]; ?> ">
+                            </div>
+                            <div class="div-label">
+                                <label for="">Sobrenome</label>
+                                <input type="text" value="<?php echo $row["sobrenome"]; ?> ">
+                            </div>
+                            <div class="div-label">
+                                <label for="">E-mail</label>
+                                <input type="text" value="<?php echo $row["email"]; ?> ">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="div2">
+                        <div class="input-container2">
+                            <div class="div-label">
+                                <label for="">Telefone</label>
+                                <input type="text" value="<?php echo $row["telefone"]; ?> ">
+                            </div>
+                            <div class="div-label">
+                                <label for="">Matrícula</label>
+                                <input type="text" value="<?php echo $row["telefone"]; ?> ">
+                            </div>
+                        </div>
+                        <div class="input-container">
+                            <div class="div-label">
+                                <label for="">Telefone</label>
+                                <input type="text" value="<?php echo $row["telefone"]; ?> ">
+                            </div>
+                            <div class="div-label">
+                                <label for="">Matrícula</label>
+                                <input type="text" value="<?php echo $row["telefone"]; ?> ">
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="img-container">
-                    <div class="div-label">
-                        <label for="nome">Nome </label>
-                        <input type="text" value="<?php echo $row["nome"]; ?> ">
-                    </div>
-                    <div class="div-label">
-                        <label for="">Sobrenome</label>
-                        <input type="text" value="<?php echo $row["sobrenome"]; ?> ">
-                    </div>
-                    <div class="div-label">
-                        <label for="">E-mail</label>
-                        <input type="text" value="<?php echo $row["email"]; ?> ">
-                    </div>
+                <div>
+                    <input type="submit" value="Cancelar">
+                    <input type="submit" value="Salvar Alterações">
+
                 </div>
-            </div>
-            <div class="div2">
-                <div class="img-container">
-                    <div class="div-label">
-                        <label for="">Telefone</label>
-                        <input type="text" value="<?php echo $row["telefone"]; ?> ">
-                    </div>
-                    <div class="div-label">
-                        <label for="">Matrícula</label>
-                        <input type="text" value="<?php echo $row["telefone"]; ?> ">
-                    </div>
-                </div>
-            </div>
+            </form>
+            
+
 
         <?php
     }
