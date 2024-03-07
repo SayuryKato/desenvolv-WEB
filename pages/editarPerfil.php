a7c558254e68f06ff62ac296b73c93d516104685
Joao Filho <joaossfbae@gmail.com>
2024-03-06 20:05:17 -0400

    tela de perfil

---
+++ pages/editarPerfil.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante Universitário</title>
+    <link rel="stylesheet" href="../style/perfil.css">
+    <link rel="stylesheet" href="../global.css">
+</head>
+<body>
+    <?php
+    // Inclui o arquivo de conexão
+    require_once '../function/consultaPerfil.php';
+    foreach ($results as $row) {
+    ?>
+        <main class="main-container">
+            <div class="aside">
+                <?php 
+                    include("../components/aside.php");
+                ?>
+                
+            </div>
+            <h1 style="display: block; width: 100%">Meu perfil</h1>
+            <div class="div1">
+                <div class="img-container">
+                    <figure>
+                        <img class="perfil-img" src="../assets/perfil_joao.jpg" alt="img_joao">
+                    </figure>
+                    <a href="editarPerfil.php" class="button">Editar Perfil</a>
+                </div>
+                <div class="img-container">
+                    <div class="div-label">
+                        <label for="nome">Nome </label>
+                        <input type="text" value="<?php echo $row["nome"]; ?> ">
+                    </div>
+                    <div class="div-label">
+                        <label for="">Sobrenome</label>
+                        <input type="text" value="<?php echo $row["sobrenome"]; ?> ">
+                    </div>
+                    <div class="div-label">
+                        <label for="">E-mail</label>
+                        <input type="text" value="<?php echo $row["email"]; ?> ">
+                    </div>
+                </div>
+            </div>
+            <div class="div2">
+                <div class="img-container">
+                    <div class="div-label">
+                        <label for="">Telefone</label>
+                        <input type="text" value="<?php echo $row["telefone"]; ?> ">
+                    </div>
+                    <div class="div-label">
+                        <label for="">Matrícula</label>
+                        <input type="text" value="<?php echo $row["telefone"]; ?> ">
+                    </div>
+                </div>
+            </div>
+
+        <?php
+    }
+        ?>
+</body>
+</main>
+
+</html>
