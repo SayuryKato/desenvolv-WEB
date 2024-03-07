a9244c0d99ab1c5c44beb373bfd1efe971703fa2
Joao Mateus <jmbp.ecp19@uea.edu.br>
2024-03-06 20:29:22 -0400

    tela de cardapio

---
+++ pages/cardapio.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cardapio RU - Cardapio</title>
+    <link rel="stylesheet" href="../style/cardapio.css"> 
+    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="../style/bootstrap.min.css">
+     
+   
+</head>
+<body>
+  <main class="containerDivisao">
+
+
+    <div class="aside">
+      <?php include "../components/aside.php" ?>
+    </div>
+
+    <div class="search-container">
+      <input type="search" name="" id="pesquisa" placeholder="Digite algo">
+      <img class="icon-lupa" src="../assets/icons/lupa.png" alt="Custom Icon">
+    </div>
+
+    <div id="content">
+      <div class="square">
+        <div class="circle-big">
+          <img class="icon" src="../assets/icons/cafe.png" alt="Custom Icon">          
+        </div>
+        <h3>Cafe da manhã</h3>
+        <div class="circle-small">
+          <img class="icon-small" src="../assets/icons/seta.png" alt="Custom Icon">         
+        </div>
+      </div>
+      <div class="square">
+        <div class="circle-big">
+          <img class="icon" src="../assets/icons/hamburguer.png" alt="Custom Icon">          
+        </div>
+        <h3>Lanche</h3>
+        <div class="circle-small">
+          <img class="icon-small" src="../assets/icons/seta.png" alt="Custom Icon">            
+        </div>
+      </div>
+      <div class="square">
+        <div class="circle-big">
+          <img class="icon" src="../assets/icons/almoço.png" alt="Custom Icon">         
+        </div>
+        <h3>Almoço</h3>
+        <div class="circle-small">
+          <img class="icon-small" src="../assets/icons/seta.png" alt="Custom Icon">          
+        </div>
+      </div>
+      <div class="square">
+        <div class="circle-big">
+          <img class="icon" src="../assets/icons/jantar.png" alt="Custom Icon">          
+        </div>
+        <h3>Jantar</h3>
+        <div class="circle-small">
+          <img class="icon-small" src="../assets/icons/seta.png" alt="Custom Icon">           
+        </div>
+      </div>
+    </div>
+
+
+  </main>
+   
+</body>
+</html>
